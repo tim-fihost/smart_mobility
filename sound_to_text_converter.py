463df6cf72a725736c1c0b45df370cad1c68916d
Timur-Malik <92898560+tim-fihost@users.noreply.github.com>
2023-11-28 09:33:59 +0900

    Add files via upload

---
+++ sound_to_text_converter.py
@@ -0,0 +1,33 @@
+# speech to text and text to speech
+import speech_recognition as sr
+# Initialize the recognizer 
+r = sr.Recognizer() 
+while(1): 
+	
+	# Exception handling to handle
+	# exceptions at the runtime
+	try:
+		
+		# use the microphone as source for input.
+		with sr.Microphone() as source2:
+			
+			# wait for a second to let the recognizer
+			# adjust the energy threshold based on
+			# the surrounding noise level 
+			r.adjust_for_ambient_noise(source2, duration=0.5)
+			
+			#listens for the user's input 
+			audio2 = r.listen(source2)
+			
+			# Using google to recognize audio
+			MyText = r.recognize_google(audio2)
+			MyText = MyText.lower()
+
+			print(MyText)
+	except sr.RequestError as e:
+		print("Could not request results; {0}".format(e))
+		
+	except sr.UnknownValueError:
+		print("unknown error occurred")
+
+#Call MyText to get access text
